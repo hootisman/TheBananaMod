1cbfd55b2e81f17a63f033ec8778ce2298e8d1e2
hootisman <david1zalewski@gmail.com>
2023-07-23 00:55:14 -0400

    recipe flour

--- src/main/java/net/hootisman/bananas/recipe/FlourRecipe.java
+++ src/main/java/net/hootisman/bananas/recipe/FlourRecipe.java
@@ -1,36 +1,101 @@
 package net.hootisman.bananas.recipe;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.mojang.logging.LogUtils;
+import net.minecraft.core.NonNullList;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.CraftingBookCategory;
-import net.minecraft.world.item.crafting.CustomRecipe;
-import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.level.Level;
 
-public class FlourRecipe extends CustomRecipe {
-    public FlourRecipe(ResourceLocation p_252125_, CraftingBookCategory p_249010_) {
-        super(p_252125_, p_249010_);
+public class FlourRecipe extends ShapelessRecipe {
+
+    private String group;
+    private CraftingBookCategory category;
+    private NonNullList<Ingredient> ingredients;
+    private ItemStack result;
+
+    public FlourRecipe(ResourceLocation location, String group, CraftingBookCategory category, ItemStack stack, NonNullList<Ingredient> ingredients) {
+        super(location, group, category, stack, ingredients);
+        this.group = group;
+        this.category = category;
+        this.result = stack;
+        this.ingredients = ingredients;
     }
 
     @Override
-    public boolean matches(CraftingContainer p_44002_, Level p_44003_) {
-        return false;
+    public NonNullList<ItemStack> getRemainingItems(CraftingContainer craftingContainer) {
+        NonNullList<ItemStack> list = NonNullList.withSize(craftingContainer.getContainerSize(), ItemStack.EMPTY);
+
+        for (int i = 0; i < list.size(); ++i){
+            ItemStack item = craftingContainer.getItem(i);
+            if (item.hasCraftingRemainingItem()){
+                LogUtils.getLogger().info("REMAINING ITEM IDK " + item.getCraftingRemainingItem().getDisplayName());
+                list.set(i, item.getCraftingRemainingItem());
+            }
+        }
+
+        return list;
     }
 
-    @Override
-    public ItemStack assemble(CraftingContainer p_44001_, RegistryAccess p_267165_) {
-        return null;
-    }
+    public static class Serializer implements RecipeSerializer<FlourRecipe>{
+        private static final ResourceLocation NAME = new ResourceLocation("minecraft", "crafting_shapeless");
+        @Override
+        public FlourRecipe fromJson(ResourceLocation p_44290_, JsonObject p_44291_) {
+            String s = GsonHelper.getAsString(p_44291_, "group", "");
+            CraftingBookCategory craftingbookcategory = CraftingBookCategory.CODEC.byName(GsonHelper.getAsString(p_44291_, "category", (String)null), CraftingBookCategory.MISC);
+            NonNullList<Ingredient> nonnulllist = itemsFromJson(GsonHelper.getAsJsonArray(p_44291_, "ingredients"));
+            if (nonnulllist.isEmpty()) {
+                throw new JsonParseException("No ingredients for shapeless recipe");
+            } else {
+                ItemStack itemstack = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(p_44291_, "result"));
+                return new FlourRecipe(p_44290_, s, craftingbookcategory, itemstack, nonnulllist);
+            }
+        }
+        private static NonNullList<Ingredient> itemsFromJson(JsonArray p_44276_) {
+            NonNullList<Ingredient> nonnulllist = NonNullList.create();
 
-    @Override
-    public boolean canCraftInDimensions(int p_43999_, int p_44000_) {
-        return false;
-    }
+            for(int i = 0; i < p_44276_.size(); ++i) {
+                Ingredient ingredient = Ingredient.fromJson(p_44276_.get(i), false);
+                if (true || !ingredient.isEmpty()) { // FORGE: Skip checking if an ingredient is empty during shapeless recipe deserialization to prevent complex ingredients from caching tags too early. Can not be done using a config value due to sync issues.
+                    nonnulllist.add(ingredient);
+                }
+            }
 
-    @Override
-    public RecipeSerializer<?> getSerializer() {
-        return null;
+            return nonnulllist;
+        }
+
+        public FlourRecipe fromNetwork(ResourceLocation p_44293_, FriendlyByteBuf p_44294_) {
+            String s = p_44294_.readUtf();
+            CraftingBookCategory craftingbookcategory = p_44294_.readEnum(CraftingBookCategory.class);
+            int i = p_44294_.readVarInt();
+            NonNullList<Ingredient> nonnulllist = NonNullList.withSize(i, Ingredient.EMPTY);
+
+            for(int j = 0; j < nonnulllist.size(); ++j) {
+                nonnulllist.set(j, Ingredient.fromNetwork(p_44294_));
+            }
+
+            ItemStack itemstack = p_44294_.readItem();
+            return new FlourRecipe(p_44293_, s, craftingbookcategory, itemstack, nonnulllist);
+        }
+
+
+        public void toNetwork(FriendlyByteBuf p_44281_, FlourRecipe p_44282_) {
+            p_44281_.writeUtf(p_44282_.group);
+            p_44281_.writeEnum(p_44282_.category);
+            p_44281_.writeVarInt(p_44282_.ingredients.size());
+
+            for(Ingredient ingredient : p_44282_.ingredients) {
+                ingredient.toNetwork(p_44281_);
+            }
+
+            p_44281_.writeItem(p_44282_.result);
+        }
     }
 }
--- src/main/java/net/hootisman/bananas/registry/BananaRecipes.java
+++ src/main/java/net/hootisman/bananas/registry/BananaRecipes.java
@@ -2,8 +2,8 @@ package net.hootisman.bananas.registry;
 
 import net.hootisman.bananas.BananaCore;
 import net.hootisman.bananas.recipe.BreadRecipe;
-import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraft.world.item.crafting.SimpleCookingSerializer;
+import net.hootisman.bananas.recipe.FlourRecipe;
+import net.minecraft.world.item.crafting.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,6 +12,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class BananaRecipes {
     public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, BananaCore.MODID);
     public static final RegistryObject<RecipeSerializer<BreadRecipe>> BREAD_BAKING_SERIALIZER = RECIPE_SERIALIZERS.register("bread_baking_serializer", () -> new SimpleCookingSerializer<BreadRecipe>(BreadRecipe::new, 200));
+    public static final RegistryObject<RecipeSerializer<FlourRecipe>> FLOUR_SERIALIZER = RECIPE_SERIALIZERS.register("flour_serializer", FlourRecipe.Serializer::new);
     public static void register(IEventBus b){
         RECIPE_SERIALIZERS.register(b);
     }
